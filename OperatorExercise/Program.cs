87fb8520f2ac35b5bd56f0cfcd96883bc57f46e7
leo davis <leob7v@gmail.com>
2023-06-24 17:50:49 -0700

    initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace OperatorExercise
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OperatorExercise
 {
-    public class Program
+    class program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //For division, create two integer variables
+
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+
+            Console.WriteLine(a + "/" + b + "is" + quotient + "remainder" + remainder);
+
+            Console.WriteLine("Please enter the radius of your circle:");
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(userInput);
+
+            Console.WriteLine(AreaOfCircle(radius));
+        }
+
+            //define a method which performs a functionality that can be used over and over again
+            public static double AreaOfCircle(double radius)
+        { 
+            //this is the scope of the method
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
         }
     }
 }
